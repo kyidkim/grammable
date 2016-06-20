d38707a3878027ced2d8f9283724a64708d6044b
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-06-20 20:18:44 +0000

    render_not_found refactor WIP

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,9 +1,19 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    
+    @gram.delete
+    redirect_to root_path
+  end
   
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     
     @gram.update_attributes(gram_params)
     
@@ -38,6 +48,7 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
   end
   
   private
@@ -46,7 +57,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
   
-  def render_not_found
-    render text: 'Not found :(', status: :not_found
+  def render_not_found(status=:not_found)
+    render text: "#{status.to_s.titleize} :(", status: :status
   end
 end

c097a84986759fcb2099b9c80ade1dcb1f70d57d
Kimberly Vandegrift <kyidmnmaiv@gmail.com>
2016-06-23 00:18:50 +0000

    comments create feature WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  
+  def create
+    redirect_to root_path
+    return render_not_found if @gram.blank?
+  end
+  
+  private
+  def render_not_found(status=:not_found)
+    render text: "#{status.to_s.titleize} :(", status: status
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Grammable::Application.routes.draw do
   devise_for :users
   root "grams#index"
   resources :grams
+  resources :comments, only: [:create]
 end
